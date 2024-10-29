1adb862f85da5f6de89c2d9fd7a3fc2e204f5cb9
Amir Zand <amirzv@outlook.com>
2024-10-29 12:14:45 +0100

    Create a context and its provider to access authentication functionalities

--- kiruna-explorer-client/src/main.jsx
+++ kiruna-explorer-client/src/main.jsx
@@ -1,5 +1,3 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
 import ReactDOM from "react-dom/client";
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -9,11 +7,14 @@ import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "./App.css";
+import { UserProvider } from "./contexts/UserContext.mjs";
 
 const router = createBrowserRouter([{ path: "/*", element: <App /> }]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <UserProvider>
+      <RouterProvider router={router} />
+    </UserProvider>
   </React.StrictMode>
 );

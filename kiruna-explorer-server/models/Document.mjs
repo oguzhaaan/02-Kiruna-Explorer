90342cbeeae592f18a4cb5629c9c707e0365b151
mick <miperseo@gmail.com>
2024-10-30 19:44:53 +0100

    add comment to docuemnt class

--- kiruna-explorer-server/models/Document.mjs
+++ kiruna-explorer-server/models/Document.mjs
@@ -1,3 +1,16 @@
+/**
+ * class representing a Document viewable on the kiruna explorer map
+ * 
+ * @param {number} id 
+ * @param {string} title
+ * @param {string} stakeholders
+ * @param {string} date
+ * @param {string} type
+ * @param {string} language
+ * @param {string} description
+ * @param {number} areaid
+ */
+
 export default class Document {
     constructor(id, title,stakeholders, date, type, language, description, areaid) {
         this.id = id;

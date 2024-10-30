20b596dce73e060ccf1eb14e1738ee3ca6001fbe
mick <miperseo@gmail.com>
2024-10-30 19:41:34 +0100

    add initial document model

---
+++ kiruna-explorer-server/models/Document.mjs
@@ -0,0 +1,14 @@
+export default class Document {
+    constructor(id, title,stakeholders, date, type, language, description, areaid) {
+        this.id = id;
+        this.title = title;
+        this.stakeholders = stakeholders;
+        this.date = date;
+        this.type = type;
+        this.language = language;
+        this.description = description;
+        this.areaid = areaid;
+        
+    }
+
+}

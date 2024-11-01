a1a28a7fdd30805241efd545a3177bcf6b58dd87
mick <miperseo@gmail.com>
2024-11-01 13:12:09 +0100

    fix params handling

--- kiruna-explorer-server/routes/DocumentRoutes.mjs
+++ kiruna-explorer-server/routes/DocumentRoutes.mjs
@@ -1,13 +1,18 @@
 import express from "express";
 import DocumentDAO from "../dao/DocumentDAO.mjs";
 
+
 const router = express.Router();
+const DocumentDao = new DocumentDAO();
 
 /* GET /api/documents/:DocId */
 router.get("/:DocId", async (req, res) => {
-    const documentId = req.params.DocId;
+    
     try {
-        const document = await DocumentDAO.getDocumentById(req.params.documentId);
+        const params = req.params;
+        const documentId = params.DocId
+
+        const document = await DocumentDao.getDocumentById(documentId);
         res.json(document);
     } catch (err) {
         console.error("Error fetching document:", err);

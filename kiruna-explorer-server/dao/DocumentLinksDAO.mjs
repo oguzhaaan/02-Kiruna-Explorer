7650c66092362b7b107502c452233967b6a043cb
mick <miperseo@gmail.com>
2024-11-03 00:53:19 +0100

    add getLinksByDocumentId

--- kiruna-explorer-server/dao/DocumentLinksDAO.mjs
+++ kiruna-explorer-server/dao/DocumentLinksDAO.mjs
@@ -1,6 +1,6 @@
 import db from "../db.mjs";
 
-import Document from "../models/Document.mjs";
+import Link from "../models/Link.mjs";
 
 export default function DocumentLinksDAO() {
 
@@ -9,7 +9,18 @@ export default function DocumentLinksDAO() {
     }
 
     this.getLinksByDocumentId = (id) => {
-        query = "SELECT * FROM document_links WHERE doc1 = ?"
+        query = "SELECT * FROM document_links WHERE doc1 = ?";
+        return new Promise((resolve, reject) => {
+            db.all(query, [id, id], (err, rows) => {
+                if (err) {
+                    reject(err);
+                } else {
+                    const links = rows.map(row => new Link(row.id, row.doc1, row.doc2, row.date, row.connection));
+                    resolve(links);
+                }
+            });
+        });
+
     
     }
 }

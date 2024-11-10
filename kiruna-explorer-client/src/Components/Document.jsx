80dbfbd8e43ec523681c9f869453cf1284dd95ac
C. Emanuele Desantis <vocallist@live.it>
2024-11-10 10:24:24 +0100

    added light theme in Document (not complete)

--- kiruna-explorer-client/src/Components/Document.jsx
+++ kiruna-explorer-client/src/Components/Document.jsx
@@ -23,7 +23,7 @@ function Document(props) {
     const customDropdownStyles = {
         control: (provided, state) => ({
             ...provided,
-            backgroundColor: '#d9d9d93f',     // Sfondo del campo
+            backgroundColor: isDarkMode ? '#d9d9d93f' : '#FFFFFFE2',     // Sfondo del campo
             borderRadius: '0.375rem',         // Bordo arrotondato come richiesto
             borderColor: 'transparent',        // Bordo attivo se selezionato
             padding: '0px 5px',               // Padding interno per centrare il testo
@@ -39,7 +39,6 @@ function Document(props) {
         }),
         placeholder: (provided) => ({
             ...provided,
-            color: 'white',                   // Placeholder bianco
         }),
         dropdownIndicator: () => ({
             display: 'none',                  // Rimuove la freccia predefinita
@@ -64,7 +63,6 @@ function Document(props) {
         option: (provided, state) => ({
             ...provided,
             backgroundColor: state.isSelected ? '#373737a6' : '#373737a6',  // Sfondo opzione selezionata
-            color: 'white',                   // Testo bianco
             cursor: 'pointer',
             fontSize: '1rem',                 // Aumenta la dimensione del testo delle opzioni
             '&:hover': {
@@ -296,7 +294,7 @@ function Document(props) {
 
     return (
         <div className={isDarkMode ? 'dark' : 'light'}>
-            <div className="bg-background_color dark:bg-red-500 min-h-screen flex flex-row justify-center">
+            <div className="bg-background_color_white dark:bg-background_color min-h-screen flex flex-row justify-center">
                 <SingleDocument setNavShow={props.setNavShow} setMode={props.setMode}
                                 setoriginalDocId={props.setoriginalDocId}></SingleDocument>
                 <Alert message={alertMessage[0]} type={alertMessage[1]}
@@ -305,30 +303,30 @@ function Document(props) {
 
                     <div className="flex flex-row items-center ml-14 gap-3">
                         <div className="z-[0] relative">
-                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-black">
+                            <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-black_text">
                                 <i className="bi bi-search"></i>
                             </span>
                             <input
                                 type="text"
                                 placeholder="Search"
-                                className="bg-search_color w-60 py-2 pl-10 pr-4 text-black rounded-[50px] focus:outline-none placeholder-black"
+                                className="border-1 border-background_color bg-search_dark_color w-60 py-2 pl-10 pr-4 text-black_text rounded-[50px] focus:outline-none placeholder-black_text"
                             />
                         </div>
 
-                        <button className="text-white text-2xl">
+                        <button className="text-black_text dark:text-white_text text-2xl">
                             <i className="bi bi-sort-down-alt"></i>
                         </button>
                     </div>
 
                     <div className="flex flex-row justify-content-end gap-3 align-items-center">
                         <button onClick={toggleModal}
-                                className="bg-[#2E6A8E] hover:bg-[#2E6A8E66] transition text-white grid justify-items-end py-2 px-4 rounded-md">
+                                className="bg-primary_color_light dark:bg-primary_color_dark hover:bg-[#2E6A8E66] transition text-black_text dark:text-white_text grid justify-items-end py-2 px-4 rounded-md">
                             <span className="text-base"><i className="bi bi-file-earmark-plus"></i> Add document</span>
                         </button>
 
                             {/* Change Theme Button */}
                         <button
-                            className="text-white grid justify-items-center transition-transform transform hover:scale-105 active:scale-95"
+                            className="text-black_text dark:text-white_text grid justify-items-center transition-transform transform hover:scale-105 active:scale-95"
                             onClick={() => {
                                 toggleTheme();
                             }}>
@@ -341,7 +339,7 @@ function Document(props) {
                         </button>
 
                         <button
-                            className="bg-[#2E6A8E] justify-items-end text-white py-2 px-5 rounded-md"
+                            className="bg-[#2E6A8E] justify-items-end text-black_text dark:text-white_text py-2 px-5 rounded-md"
                             onClick={() => {
                                 navigate("/documents/1")
                             }}>
@@ -352,29 +350,29 @@ function Document(props) {
             </div>
 
             {props.isModalOpen && (
-                <div className="py-4 fixed inset-0 flex items-center justify-center scrollbar-thin scrollbar-webkit">
+                <div className={`${isDarkMode ? "dark-select" : "light-select"} py-4 fixed inset-0 flex items-center justify-center scrollbar-thin scrollbar-webkit`}>
 
                     <div
-                        className="bg-box_color backdrop-blur-2xl drop-shadow-xl w-3/6 p-6 h-full overflow-y-auto rounded-lg flex flex-col items-center relative scrollbar-thin scrollbar-webkit">
-                        <h2 className="text-white text-xl font-bold ">Add New Document</h2>
+                        className="bg-box_white_color dark:bg-box_color backdrop-blur-2xl drop-shadow-xl w-3/6 p-6 h-full overflow-y-auto rounded-lg flex flex-col items-center relative scrollbar-thin scrollbar-webkit">
+                        <h2 className="text-black_text dark:text-white_text text-xl font-bold ">Add New Document</h2>
                         <button onClick={toggleModal}
-                                className="absolute top-5 text-white right-4 hover:text-gray-600">
+                                className="absolute top-5 text-black_text dark:text-white_text right-4 hover:text-gray-600">
                             <i className="bi bi-x-lg text-2xl"></i>
                         </button>
 
                         <div className="input-title mb-4 w-full">
-                            <label className="text-white w-full ml-2 mb-1 text-base text-left">Title*</label>
+                            <label className="text-black_text dark:text-white_text w-full ml-2 mb-1 text-base text-left">Title*</label>
                             <input
                                 type="text"
                                 placeholder="Title"
                                 value={props.newDocument.title}
                                 onChange={handleTitle}
-                                className={`w-full px-3 text-base py-2 text-white placeholder:text-placeholder_color bg-input_color rounded-md ${errors.title ? 'border-red-500 border-1' : ''}`}
+                                className={`w-full px-3 text-base py-2 text-black_text dark:text-white_text placeholder:text-placeholder_color bg-input_color_light dark:bg-input_color_dark rounded-md ${errors.title ? 'border-red-500 border-1' : ''}`}
                             />
                         </div>
 
                         <div className="input-stakeholder mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Stakeholders*</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Stakeholders*</label>
                             <Select
                                 isMulti
                                 options={stakeholderOptions}
@@ -385,17 +383,17 @@ function Document(props) {
                                 placeholder="None"
                                 isClearable={false}
                                 isSearchable={false}
-                                className="select"
+                                className="select text-black_text dark:text-white_text"
                             />
                         </div>
 
                         <div className="input-scale mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Scale*</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Scale*</label>
                             <select
                                 id="document-type"
                                 value={props.newDocument.scale}
                                 onChange={handleScale}
-                                className={`w-full px-3 text-base py-2 text-white bg-input_color rounded-md ${errors.scale ? 'border-red-500 border-1' : ''}`}>
+                                className={`w-full px-3 text-base py-2 text-text-black_text dark:text-white_text bg-input_color_light dark:bg-input_color_dark rounded-md ${errors.scale ? 'border-red-500 border-1' : ''}`}>
                                 <option value="none">None</option>
                                 <option value="text">Text</option>
                                 <option value="concept">Concept</option>
@@ -406,7 +404,7 @@ function Document(props) {
 
                         {props.newDocument.scale === 'plan' &&
                             <div className="input-plan mb-4 w-full">
-                                <label className="text-white mb-1 text-base w-full ml-2 text-left">Enter the scale
+                                <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Enter the scale
                                     1:n</label>
                                 <input
                                     id="number-input"
@@ -414,29 +412,29 @@ function Document(props) {
                                     value={props.newDocument.planNumber}
                                     placeholder="n"
                                     onChange={handlePlanNumber}
-                                    className={`w-full text-base px-3 py-2 text-white bg-input_color rounded-md ${errors.planNumber ? 'border-red-500 border-1' : ''}`}>
+                                    className={`w-full text-base px-3 py-2 text-text-black_text dark:text-white_text bg-input_color_light dark:bg-input_color_dark rounded-md ${errors.planNumber ? 'border-red-500 border-1' : ''}`}>
                                 </input>
                             </div>}
 
                         <div className="input-date mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Issuance
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Issuance
                                 date*</label>
                             <input
                                 id="document-date"
                                 type="date"
                                 value={props.newDocument.date}
                                 onChange={handleDate}
-                                className={`w-full px-3 text-base py-2 text-placeholder_color text-white placeholder:text-placeholder_color bg-input_color rounded-md  ${errors.date ? 'border-red-500 border-1' : ''}`}>
+                                className={`w-full px-3 text-base py-2 text-placeholder_color text-text-black_text dark:text-white_text placeholder:text-placeholder_color bg-input_color_light dark:bg-input_color_dark rounded-md  ${errors.date ? 'border-red-500 border-1' : ''}`}>
                             </input>
                         </div>
 
                         <div className="input-type mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Type*</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Type*</label>
                             <select
                                 id="document-type"
                                 value={props.newDocument.type}
                                 onChange={handleType}
-                                className={`w-full px-3 text-base py-2 text-placeholder_color text-white placeholder:text-placeholder_color bg-input_color rounded-md ${errors.type ? 'border-red-500 border-1' : ''}`}>
+                                className={`w-full px-3 text-base py-2 text-black_text dark:text-white_text bg-input_color_light dark:bg-input_color_dark rounded-md ${errors.type ? 'border-red-500 border-1' : ''}`}>
                                 <option value="none">None</option>
                                 <option value="design">Design document</option>
                                 <option value="informative">Informative document</option>
@@ -450,12 +448,12 @@ function Document(props) {
                         </div>
 
                         <div className="input-language mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Language</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Language</label>
                             <select
                                 id="document-language"
                                 value={props.newDocument.language}
                                 onChange={handleLanguage}
-                                className="w-full px-3 text-base py-2 text-placeholder_color text-white placeholder:text-placeholder_color bg-input_color rounded-md">
+                                className="w-full px-3 text-base py-2 text-placeholder_color text-black_text dark:text-white_text placeholder:text-placeholder_color bg-input_color_light dark:bg-input_color_dark rounded-md">
                                 <option value="">None</option>
                                 {popularLanguages.map((lang) => (
                                     <option key={lang.code} value={lang.code}>
@@ -467,31 +465,31 @@ function Document(props) {
 
 
                         <div className="input-number mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Pages</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Pages</label>
                             <input
                                 id="number-input"
                                 type="number"
                                 value={props.newDocument.pages}
                                 placeholder="Select a number"
                                 onChange={handleNumber}
-                                className="w-full text-base px-3 py-2 text-placeholder_color text-white placeholder:text-placeholder_color bg-input_color rounded-md">
+                                className="w-full text-base px-3 py-2 text-placeholder_color text-black_text dark:text-white_text placeholder:text-placeholder_color bg-input_color_light dark:bg-input_color_dark rounded-md">
                             </input>
                         </div>
 
                         <div className="input-description mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Description*</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Description*</label>
                             <textarea
                                 placeholder="Description"
                                 value={props.newDocument.description}
                                 onChange={handleDescription}
-                                className={`w-full p-2 px-3 py-2 text-base text-white border-gray-300 placeholder:text-placeholder_color bg-input_color ${errors.description ? 'border-red-500 border-1' : ''}`}
+                                className={`w-full p-2 px-3 py-2 text-base text-text-black_text dark:text-white_text border-gray-300 placeholder:text-placeholder_color bg-input_color_light dark:bg-input_color_dark ${errors.description ? 'border-red-500 border-1' : ''}`}
                                 rows="4"
                             ></textarea>
                         </div>
 
                         <div className="input-map mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Georeference</label>
-                            {props.newAreaId && <label className="text-white text-base w-full text-left py-1"><i
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Georeference</label>
+                            {props.newAreaId && <label className="text-black_text dark:text-white_text text-base w-full text-left py-1"><i
                                 className="bi bi-check-lg align-middle text-green-400"></i> You
                                 selected {props.newAreaId === 1 ? "Municipality Area" : `Area N. ${props.newAreaId}`}
                             </label>}
@@ -499,13 +497,13 @@ function Document(props) {
                                 onClick={() => {
                                     navigate("/map")
                                 }}
-                                className="w-full p-2 text-base text-black border border-gray-300 focus:outline-none bg-[#D9D9D9] rounded-md">
+                                className="w-full p-2 text-base text-white_text dark:text-black_text border border-gray-300 focus:outline-none bg-customGray1 dark:bg-[#D9D9D9] rounded-md">
                                 <i className="bi bi-globe-europe-africa"></i> Open the Map
                             </button>
                         </div>
 
                         <div className="input-link mb-4 w-full">
-                            <label className="text-white mb-1 text-base w-full ml-2 text-left">Linking</label>
+                            <label className="text-black_text dark:text-white_text mb-1 text-base w-full ml-2 text-left">Linking</label>
                             {
                                 props.connections && props.connections.length > 0 &&
                                 <p className="m-0 px-2 py-1 text-gray-500">{(props.connections.length) + " connections selected"}</p>
@@ -516,14 +514,14 @@ function Document(props) {
                                     props.setMode("return");
                                     navigate("/linkDocuments");
                                 }}
-                                className="w-full p-2 text-black border text-base border-gray-300 focus:outline-none bg-[#D9D9D9] rounded-md"> Select
+                                className="w-full p-2 text-white_text dark:text-black_text border text-base border-gray-300 focus:outline-none bg-customGray1 dark:bg-[#D9D9D9] rounded-md"> Select
                                 Documents to Link
                             </button>
                         </div>
 
                         {/* Save button */}
                         <button
-                            className="bg-[#1A5F88] w-full font-bold text-lg  text-white py-2 px-4 rounded-lg mt-4"
+                            className="bg-primary_color_dark w-full font-bold text-lg text-white_text py-2 px-4 rounded-lg mt-4"
                             onClick={handleConfirm}>
                             Confirm
                         </button>

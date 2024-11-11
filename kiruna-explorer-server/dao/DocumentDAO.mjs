94e9c4fc7c0deebb0165c982398f8ee1fe6ec69f
oguzhaaan <oguzhan_akgun_1999@hotmail.com>
2024-11-11 13:20:46 +0100

    Minor changes for code readability

--- kiruna-explorer-server/dao/DocumentDAO.mjs
+++ kiruna-explorer-server/dao/DocumentDAO.mjs
@@ -6,6 +6,7 @@ import { AreaNotFound } from "../models/Area.mjs";
 import { InvalidArea } from "../models/Area.mjs";
 
 const areaDAO = new AreaDAO();
+const documentDAO = new DocumentDAO();
 export default function DocumentDAO() {
     this.getAllDocuments = () => {
         const query = "SELECT * FROM document";
@@ -62,23 +63,6 @@ export default function DocumentDAO() {
         });
     }
 
-    this.deleteAreaById = (id) => {
-        const query = "DELETE FROM areas WHERE id = ?";
-    
-        return new Promise((resolve, reject) => {
-            db.run(query, [id], function (err) {
-                if (err) {
-                    return reject(err);
-                }
-                // Check if any row was affected (i.e., if the area was deleted)
-                if (this.changes === 0) {
-                    return reject(new AreaNotFound());
-                }
-                resolve("Area deleted successfully.");
-            });
-        });
-    };
-
     this.updateDocumentAreaId = (documentId, newAreaId) => {
         return new Promise((resolve, reject) => {
             // Fetch oldAreaId, areaIdsInDoc, and allAreas at the same time
@@ -109,14 +93,10 @@ export default function DocumentDAO() {
     
                         if (!updatedAreaIdsInDoc.includes(oldAreaId)) {
                             // If oldAreaId is no longer in use, delete it from the area table
-                            const deleteQuery = "DELETE FROM area WHERE id = ?";
-                            db.run(deleteQuery, [oldAreaId], (deleteErr) => {
-                                if (deleteErr) {
-                                    return reject(deleteErr);
-                                }
-                                console.log("Unused area deleted successfully.");
-                                resolve(true);
-                            });
+                            Promise.resolve([
+                                areaDAO.deleteAreaById(oldAreaId),
+                                resolve(true)
+                            ]);
                         } else {
                             console.log("Area ID is still in use; not deleted.");
                             resolve(true); // Resolve without deleting if still in use

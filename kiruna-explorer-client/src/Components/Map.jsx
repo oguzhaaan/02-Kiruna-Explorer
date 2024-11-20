c2ec146b64c433ecf56ba2f7b57da1957d67fea6
Lorenzo Cuccu s330260 <s330260@studenti.polito.it>
2024-11-20 12:50:13 +0100

    map render fixed

--- kiruna-explorer-client/src/Components/Map.jsx
+++ kiruna-explorer-client/src/Components/Map.jsx
@@ -359,7 +359,7 @@ function GeoreferenceMap(props) {
                 {mapRef.current && !drawnObject && showMuniAreas && <HomeButton handleMunicipalAreas={handleMunicipalAreas} />}
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     className={isDarkMode ? "custom-tile-layer" : ""}
                 />
                 <ZoomControl position="topright" />

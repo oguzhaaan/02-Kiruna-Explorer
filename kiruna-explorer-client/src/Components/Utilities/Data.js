a3d100d69e5affcf659db2a2f0fd5f986f64cdbc
FRANCESCO CHIAIA s322348 <s322348@studenti.polito.it>
2024-12-04 12:26:07 +0100

    real values for type and stakeholders for filter menu

--- kiruna-explorer-client/src/Components/Utilities/Data.js
+++ kiruna-explorer-client/src/Components/Utilities/Data.js
@@ -1,12 +1,3 @@
-export const stakeholderOptions = [
-  { value: "lkab", label: "LKAB" },
-  { value: "municipality", label: "Municipality" },
-  { value: "regional authority", label: "Regional authority" },
-  { value: "architecture firms", label: "Architecture firms" },
-  { value: "citizens", label: "Citizens" },
-  { value: "others", label: "Others" },
-];
-
 export const popularLanguages = [
   { code: "en", name: "English" },
   { code: "sv", name: "Swedish" },
@@ -21,70 +12,4 @@ export const popularLanguages = [
   { code: "pt", name: "Portuguese" },
   { code: "ar", name: "Arabic" },
   { code: "it", name: "Italian" },
-];
-
-export const documentTypes = [
-  {
-    id: 1,
-    name: "Design",
-  },
-  {
-    id: 2,
-    name: "Informative",
-  },
-  {
-    id: 3,
-    name: "Prescriptive",
-  },
-  {
-    id: 4,
-    name: "Technical",
-  },
-  {
-    id: 5,
-    name: "Agreement",
-  },
-  {
-    id: 6,
-    name: "Conflict",
-  },
-  {
-    id: 7,
-    name: "Consultation",
-  },
-  {
-    id: 8,
-    name: "Material Effects",
-  },
-  {
-    id: 9,
-    name: "Add a new one..."
-  }
-];
-
-export const stakeholders = [
-  {
-    id: 1,
-    name: "LKAB",
-  },
-  {
-    id: 2,
-    name: "Municipality",
-  },
-  {
-    id: 3,
-    name: "Regional Authority",
-  },
-  {
-    id: 4,
-    name: "Architecture Firms",
-  },
-  {
-    id: 5,
-    name: "Citizens",
-  },
-  {
-    id: 6,
-    name: "Add a new one..."
-  }
 ];

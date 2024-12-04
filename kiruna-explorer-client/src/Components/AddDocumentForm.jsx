c3533b7e8b0c23e80de583a9fde66e2dd99014bb
FRANCESCO CHIAIA s322348 <s322348@studenti.polito.it>
2024-12-05 00:08:36 +0100

    fix reliability low

--- kiruna-explorer-client/src/Components/AddDocumentForm.jsx
+++ kiruna-explorer-client/src/Components/AddDocumentForm.jsx
@@ -339,7 +339,7 @@ const AddDocumentForm = (props) => {
       props.setnewAreaId(null);
       props.setConnections([]);
       closeForm();
-      await API.addStakeholdersToDocument(documentId.lastId,stakeholdersName);
+      await API.addStakeholdersToDocument(documentId.lastId, stakeholdersName);
     } catch (error) {
       console.log(error);
       props.setAlertMessage([error.message, "error"]);
@@ -379,8 +379,13 @@ const AddDocumentForm = (props) => {
   return (
     <div
       className={`${isDarkMode ? "dark-select" : "light-select"
-        } py-4 fixed inset-0 flex items-center justify-center scrollbar-thin scrollbar-webkit z-[1040]`}
+        } py-4 fixed inset-0 bg-red-500 flex items-center justify-center scrollbar-thin scrollbar-webkit z-[1040]`}
       onClick={closeForm}
+      onKeyUp={(e) => {
+        if (e.key === 'Enter') {
+          closeForm();
+        }
+      }}
     >
       <div
         className="bg-box_white_color dark:bg-box_color backdrop-blur-2xl shadow-xl w-1/2 px-10 py-10 h-full overflow-y-auto rounded-lg flex flex-col relative scrollbar-thin scrollbar-webkit"
